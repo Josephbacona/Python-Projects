f9ee9cf2c1e8a6a2f710c517fbdce65f1a619319
Josephbacona <85919565+Josephbacona@users.noreply.github.com>
2022-01-24 21:23:57 -0800

    added File_Transfer_Assignment.py

--- File_Transfer_Assignment.py
+++ File_Transfer_Assignment.py
@@ -1,13 +1,101 @@
 import shutil
 import os
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
+import tkinter
+from tkinter import *
 
-#set where the source of the files are
-source = 'folderA/'
+win = Tk()
 
-#set the destination path to folderB
-destination = 'folderB/'
-files = os.listdir(source)
+class ParentWindow(Frame):
+    def __init__ (self, master):
+        Frame.__init__(self)
 
+        self.master = master
+        self.master.resizable(width=False, height=False)
+        self.master.geometry('{}x{}'.format(700, 400))
+        self.master.title('Learning Tkinter!')
+        self.master.config(bg='lightgray')
+
+        self.varFName = StringVar()
+        self.varLName = StringVar()
+
+        self.lblSource = Label(self.master,text='Select Source: ', font=("Helvetia", 16), fg='black', bg='lightgray' )
+        self.lblSource.grid(row=0,column=0,padx=(30,0), pady=(30,0))
+
+        self.lblSource = Label(self.master,text='Select Source: ', font=("Helvetia", 16), fg='black', bg='lightgray' )
+        self.lblSource.grid(row=0,column=0,padx=(30,0), pady=(30,0))
+
+        # Create a source Button
+        ttk.Button.grid(row=1,column=0,padx=(30,0), pady=(30,0))(win, text="Browse", command=open_file).pack(pady=30)
+
+        self.lblDestination = Label(self.master,text='Select Destination: ', font=("Helvetia", 16), fg='black', bg='lightgray' )
+        self.lblDestination.grid(row=2,column=0,padx=(30,0), pady=(30,0))
+
+        # Create a destination Button
+        ttk.Button.grid(row=3,column=0,padx=(30,0), pady=(30,0))(win, text="Browse", command=open_file).pack(pady=30)
+
+        self.lblDisplay = Label(self.master,text='', font=("Helvetia", 16), fg='black', bg='lightgray')
+        self.lblDisplay.grid(row=3,column=1,padx=(30,0), pady=(30,0))
+
+        self.txtSource = Entry(self.master,text=self.varFName, font=("Helvetia", 16), fg='black', bg='lightblue')
+        self.txtSource.grid(row=0,column=1,padx=(30,0), pady=(30,0))
+        
+        self.txtDestination = Entry(self.master,text=self.varLName, font=("Helvetia", 16), fg='black', bg='lightblue')
+        self.txtDestination.grid(row=1,column=1,padx=(30,0), pady=(30,0))
+
+        self.btnSubmit = Button(self.master, text="Move Files", width=10, height=2)
+        self.btnSubmit.grid(row=2,column=1,padx=(30,0), pady=(30,0), sticky=NE)
+
+     
+
+
+def open_file():
+   file = filedialog.askopenfile(mode='r', filetypes=[('Python Files', '*.py')])
+   if file:
+      content = file.read()
+      file.close()
+      print("%d characters in this file" % len(content))
+
+win.mainloop()
+
+
+
+def select_file():
+    filetypes = (
+        ('text files', '*.txt'),
+        ('All files', '*.*')
+    )
+    filename = fd.askopenfilename(
+        title='Open a file',
+        initialdir='/',
+        filetypes=filetypes)
+    showinfo(
+        title='Selected File',
+        message=filename
+    )
+
+def new_Files():
+    filetypes = (
+        ('text files', '*.txt'),
+        ('All files', '*.*')
+    )
+    filename = fd.askopenfilename(
+        title='new_Files',
+        initialdir='/',
+        filetypes=filetypes)
+    showinfo(
+        title='New Files',
+        message=filename
+    )
+
+if __name__ == "__main__":
+    root = Tk()
+    App = ParentWindow(root)
+    root.mainloop()
+'''
 for i in files:
     #we are saying move the files represented by 'i' to their new destination
     shutil.move(source+i, destination)
@@ -19,10 +107,11 @@ source2 = 'newFiles/'
 destination2 = 'newFilesCopied/'
 newfiles = os.listdir(source2)
 
+
+
 for i in newfiles:
     #we are saying move the newly created or modified files repressented
     #by 'i' to their destination
     shutil.move(source2+i, destination2)
-
-
+'''
 
